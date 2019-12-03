105d041f5354bdb06fab538eba5a06e6b9c419df
fragilis <vitafragilis@gmail.com>
2019-12-03 19:53:59 +0900

    確認画面を途中まで実装

--- enquetes/crud.js
+++ enquetes/crud.js
@@ -130,7 +130,7 @@ router.post('/add',
     check('detail').isLength({ max: 1000 }),
     check('answers').exists().isLength({ min: 1, max: 10 }),
     check('answers.*').exists().isString().isLength({ max: 100 }),
-    check('answer_type').exists().isIn(['radio', 'check']),
+    check('answer_type').exists().isIn(['radio', 'checkbox']),
     check('hashtags').isLength({ max: 200 }),
     check('period_hours').exists().isIn(['-1', '6', '12', '24', '48', '72', '168']),
     check('publish_status').exists().isIn(['1', '2'])
